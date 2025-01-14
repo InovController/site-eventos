b33620aad9e4a1688bf975508d97c8495dadfda6
Davi <inovacao@controller-rnc.com.br>
2025-01-14 14:48:15 -0300

    swap sqlite to postgresql

--- app/settings.py
+++ app/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'site_eventos',
+        'USER': 'postgres',
+        'PASSWORD': 'Inov@1600',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
