d55304d6254c796748f3654d18581c881387a53c
Davi <inovacao@controller-rnc.com.br>
2025-04-10 16:55:23 -0300

    Adjusted presence validation redirection after login and registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,31 +1,29 @@
 from django.contrib.auth.views import LoginView
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout
 from django.contrib import messages
 from django.shortcuts import render, redirect   
 from django.views import View
 from users.forms import CustomUserCreationForm, CustomAuthenticationForm
-from django.urls import reverse_lazy
 
 
 class RegisterView(View):
 
     def get(self, request):
         user_form = CustomUserCreationForm()
-        url_name = request.resolver_match.url_name
-        return render(request, 'register.html', {'user_form': user_form, 'url_name': url_name})
+        next_url = request.GET.get('next', '')
+        return render(request, 'register.html', {'user_form': user_form, 'next': next_url})
 
     
     def post(self, request):
         user_form = CustomUserCreationForm(request.POST)
+        next_url = request.POST.get('next') or '/'
         if user_form.is_valid():
-            user_form.save()
-
-            messages.success(request, "Cadastro realizado com sucesso! Você já pode fazer login.")
-            return redirect('login')
+            user = user_form.save()
+            login(request, user)
+            return redirect(next_url)
             
         messages.error(request, "O formulário contém erros. Por favor, corrija os campos destacados.")
-        url_name = request.resolver_match.url_name
-        return render(request, 'register.html', {'user_form': user_form, 'url_name': url_name})
+        return render(request, 'register.html', {'user_form': user_form, 'next': next_url,})
     
     
 class CustomLoginView(LoginView):
@@ -39,10 +37,6 @@ class CustomLoginView(LoginView):
         url_name = self.request.resolver_match.url_name
         context['url_name'] = url_name
         return context
-    
-    
-    def get_success_url(self):
-        return reverse_lazy('events_internal')
 
 
 def logout_view(request):
