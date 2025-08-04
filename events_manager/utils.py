a58ae17934c7122710d809416950a083c78a9b60
Davi <inovacao@controller-rnc.com.br>
2025-08-04 15:51:19 -0300

    update certificate logic

--- events_manager/utils.py
+++ events_manager/utils.py
@@ -2,10 +2,8 @@ import secrets
 import string
 import qrcode
 from io import BytesIO
-from django.http import HttpResponse
 from django.core.files import File
 from django.conf import settings
-from PIL import Image, ImageDraw, ImageFont
 
 
 def generate_short_token(length=6):
@@ -27,40 +25,3 @@ def generate_event_qr_code(event):
 
     # Save to the ImageField of the event
     event.qrcode.save(f'qrcode_{event.id}.png', File(qr_io))
-
-
-def generate_certificate_response(user, event, for_download=False):
-    user_name = f"{user.first_name} {user.last_name}"
-    certificate_path = event.certificate.url.lstrip('/')
-
-    image = Image.open(certificate_path)
-    draw = ImageDraw.Draw(image)
-
-    center_x = 525
-    y = 205
-    font_size = 18
-
-    if 33 < len(user_name) < 45:
-        font_size = 16
-    elif len(user_name) >= 45:
-        font_size = 12
-        y = 210
-
-    font = ImageFont.truetype('media/fonts/Sora-Medium.ttf', font_size)
-    name_text = user_name.upper()
-
-    bbox = draw.textbbox((0, 0), name_text, font=font)
-    text_width = bbox[2] - bbox[0]
-    position = (center_x - text_width // 2, y)
-
-    draw.text(position, name_text, font=font, fill="white")
-
-    buffer = BytesIO()
-    image.save(buffer, format='PNG')
-    buffer.seek(0)
-
-    response = HttpResponse(buffer, content_type='image/png')
-    if for_download:
-        response['Content-Disposition'] = f'attachment; filename="certificate_{user_name}.png"'
-
-    return response
