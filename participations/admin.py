da3af18aa2b50cac4c6444479ca290456b493122
Davi <inovacao@controller-rnc.com.br>
2025-09-05 11:41:04 -0300

    update certificate tab and participation admin

--- participations/admin.py
+++ participations/admin.py
@@ -4,4 +4,4 @@ from participations.models import Participation
 @admin.register(Participation)
 class ParticipationAdmin(admin.ModelAdmin):
     list_display = ('user', 'event', 'date_joined')
-    search_fields = ('title',)
+    search_fields = ('event__title', 'user__email')
