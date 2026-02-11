4938b388c3efca0fd69936ef4a70d3b1bf6240fe
Davi <inovacao@controller-rnc.com.br>
2026-02-11 15:49:14 -0300

    add evaluation template field to create and update forms

--- events_manager/forms.py
+++ events_manager/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from events_manager.models import Event
+from evaluation.models import EvaluationTemplate
 
 
 class EventModelForm(forms.ModelForm):
@@ -7,3 +8,12 @@ class EventModelForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = '__all__'
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Só templates ativos no dropdown
+        if "evaluation_template" in self.fields:
+            self.fields["evaluation_template"].queryset = EvaluationTemplate.objects.filter(is_active=True)
+            self.fields["evaluation_template"].required = False
+            self.fields["evaluation_template"].empty_label = "Sem avaliação"
